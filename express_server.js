1ee2d4ce4a397690676a3aa3258cdbb4ff111e50
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-04-13 15:47:20 -0700

    add empty randomstring function

--- express_server.js
+++ express_server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+const generateRandomString = function() {
+
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -28,6 +35,11 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req,res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req,res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
 
 app.get("/urls/:shortURL", (req,res) => {
