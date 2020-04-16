3502c082de57e7b0e561e486f6aee48c154e9990
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-04-16 15:20:20 -0700

    users can only edit and delete what they own

--- src/controller/dataController.js
+++ src/controller/dataController.js
@@ -1,12 +1,10 @@
 const urlDatabase = require('../data/urlDatabase');
 const lookUpUserbyId = require('../utils/lookUpUserById');
-const urlsforUser = require('../utils/urlsforUser');
 
 exports.delete = (req,res) => {
   const values = Object.values(urlDatabase);
 
   for (let value of values) {
-    console.log(value.userID);
     if (req.cookies.user_id === value.userID) {
       delete urlDatabase[req.params.shortURL];
       res.status(200).redirect("/");
@@ -18,14 +16,24 @@ exports.delete = (req,res) => {
 };
 
 exports.updateShortURL = (req, res) => {
+  const values = Object.values(urlDatabase);
   const user = lookUpUserbyId(req.cookies.user_id);
   let templateVars = {
     shortURL: req.params.shortURL,
     longURL: req.body.longURL,
     user
   };
-  urlDatabase[req.params.shortURL] = {longURL :templateVars.longURL, userID: req.cookies.user_id};
-  res.status(201).redirect(`/urls/${req.params.shortURL}`);
+  for (let value of values) {
+    if (req.cookies.user_id === value.userID) {
+      urlDatabase[req.params.shortURL] = {longURL :templateVars.longURL, userID: req.cookies.user_id};
+      res.status(201).redirect(`/urls/${req.params.shortURL}`);
+      return;
+    }
+  }
+  
+  res.status(403).send('this link does not belong to you! you cannot edit it');
+  return;
+  
 };
 
 exports.loginName = (req,res) => {
